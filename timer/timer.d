c8484c3d14660823731313f6407ca0c1059dcad3
SonicFreak94 <sonicfreak94@hotmail.com>
2016-06-09 14:34:40 -0600

    TAAAAAKE THIS

--- timer/timer.d
+++ timer/timer.d
@@ -1,85 +1,66 @@
 module timer;
 
-import std.typecons; // for ScopeTimer
-
 public import std.datetime;
 
+// TODO: Remove TickDuration as it will be deprecated.
 // TODO: Implement AsyncTimer (threaded, callbacks, etc)
 
 class Timer
 {
 private:
-	Duration initTime;
-	bool finished;
+	StopWatch stopwatch;
 
 public:
-	this(in Duration duration, bool startNow = false)
+	this(in Duration duration, AutoStart autostart)
 	{
-		this.duration = duration;
-		this(startNow);
-	}
-	this (bool startNow = false)
-	{
-		if (startNow)
-			start();
+		this.duration = cast(TickDuration)duration;
+		stopwatch = StopWatch(autostart);
 	}
 
-	// The duration to wait. Can be modified at any time.
-	Duration duration;
+	TickDuration duration;
 
-	// Starts or restarts the timer by setting startTime to the current system time.
-	void start()
+	@safe void reset()
 	{
-		finished = false;
-		initTime = Clock.currStdTime().hnsecs();
+		stopwatch.reset();
+	}
+	@safe void start()
+	{
+		stopwatch.start();
+	}
+	@safe void stop()
+	{
+		stopwatch.stop();
+	}
+	const @safe TickDuration peek()
+	{
+		return stopwatch.peek();
+	}
+	@safe void setMeasured(TickDuration d)
+	{
+		stopwatch.setMeasured(d);
+	}
+	const pure nothrow @property @safe bool running()
+	{
+		return stopwatch.running();
 	}
 
 	// Returns true if the elapsed time has exceeded the desired duration.
 	bool done() @property
 	{
-		return (!finished) ? finished = elapsed >= duration : finished;
-	}
+		if (!running)
+			return true;
 
-	// The time of the last call to start()
-	Duration startTime() @property
-	{
-		return initTime;
+		if (peek() >= duration)
+		{
+			stop();
+			return true;
+		}
+
+		return false;
 	}
-	// The elapsed time since startTime
-	Duration elapsed() @property
+	// Remaining time
+	auto remainder() @property
 	{
-		return (Clock.currStdTime().hnsecs - initTime);
+		return duration - peek();
 	}
 }
-
-class CodeTimer : Timer
-{
-	string formatString;
-	this(bool startNow = true, in string format = "%02dh %02dm %02ds %04dms %04dus %04dhn %04dns")
-	{
-		formatString = format;
-		super(startNow);
-	}
-	~this()
-	{
-		result();
-	}
-
-	void result()
-	{
-		import std.stdio : stdout;
-
-		if (finished)
-			return;
-
-		finished = true;
-		auto result = elapsed.split!("hours", "minutes", "seconds", "msecs", "usecs", "hnsecs", "nsecs");
-
-		stdout.writefln(formatString, result.hours, result.minutes, result.seconds,
-						result.msecs, result.usecs, result.hnsecs, result.nsecs);
-	}
-}
-
-// Convenient alias for a scoped CodeTimer.
-// Usage example: auto t = ScopeTimer();
-alias ScopeTimer = scoped!CodeTimer;
